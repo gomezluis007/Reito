afbc4636680cfb93e75823285dc686a002044d5e
vagrant <vagrant@debian10.localdomain>
2021-09-23 03:41:40 +0000

    Agregar modelo de los vehiculos

--- reito/vehiculos/models.py
+++ reito/vehiculos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from usuarios.models import Usuario
 
+class Vehiculo(models.Model):
+    modelo = models.CharField("Modelo", max_length=20)
+    marca = models.CharField("Marca", max_length=20)
+    matricula = models.CharField("Matricula", max_length=7)
+    asientos = models.IntegerField("Asientos")
+    descripcion = models.CharField("Descripcion", max_length=500)
+    id_usuario = models.OneToOneField("usuarios.Usuario", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.matricula
 # Create your models here.
