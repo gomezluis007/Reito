11ce66cb656aca643cab859fe5a4ffe40365bf36
vagrant <vagrant@debian10.localdomain>
2021-09-23 20:36:08 +0000

    Agregar backend para agregar un nuevo Destino. Agregar backend para ver el detalle de Viaje.

--- reito/viajes/forms.py
+++ reito/viajes/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.db.models import fields
-from .models import Viaje
+from .models import Viaje, Destino
 
 class ViajeForm(forms.ModelForm):
     class Neta:
         model = Viaje
+
+        fields = '__all__'
+
+class DestinoForm(forms.ModelForm):
+    class Neta:
+        model = Destino
 
         fields = '__all__'
--- reito/viajes/views.py
+++ reito/viajes/views.py
@@ -1,15 +1,25 @@
-from reito.viajes.models import Viaje
+from reito.viajes.models import Viaje, Destino
 from django.shortcuts import render
 from django.shortcuts import render
 
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 
 from django.urls import reverse_lazy
 
-from .forms import ViajeForm
+from .forms import DestinoForm, ViajeForm
 
 class NuevoViaje(CreateView):
     model = Viaje
     #extra_context = {'':''}
     form_class = ViajeForm
     success_url = reverse_lazy('viajes:detalle')
+
+class NuevoDestino(CreateView):
+    model = Destino
+    #extra_context = {'':''}
+    form_class = DestinoForm
+    success_url = reverse_lazy('viajes:detalle')
+
+class DetalleViaje(DetailView):
+    model = Viaje
