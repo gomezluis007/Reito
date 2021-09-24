2621e935a6e90095e6cb4271bdbbb89b05424e4b
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-24 13:34:12 -0500

    Agregar funcionalidad para reservar un asiento en un viaje

--- reito/reservas/views.py
+++ reito/reservas/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from usuarios.models import Usuario
+from viajes.models import Viaje
+from .models import Reserva
 
-# Create your views here.
+
+def nueva_reserva(request, user_pk, viaje_pk):
+    if(user_pk and viaje_pk):
+        viaje = Viaje.objects.get(id=viaje_pk)
+        usuario = Usuario.objects.get(id=user_pk)
+
+        if(viaje.asientos > 0):
+            reserva = Reserva.objects.create(viaje=viaje, usuario=usuario)
+            reserva.save()
+            viaje.asientos -= 1
+            viaje.save()
+            messages.success(request, "Tu asiento ha sido reservado exitosamente")
+            return redirect('viajes:index')
+        else:
+            messages.error(request, "Ya no hay asientos disponibles en este viaje")
+            return redirect('viajes:index')
