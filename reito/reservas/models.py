28dc727d6f6d02b2950808aca4592fe433390a4e
Oscar Corvera <55102833+corverita@users.noreply.github.com>
2021-09-24 11:47:59 -0500

    Update models.py

--- reito/reservas/models.py
+++ reito/reservas/models.py
@@ -3,12 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Reserva(models.Model):
-    nombre = models.CharField("Nombre", max_length=50)
-
-    def __str__(self):
-        return self.nombre
-
-class Reserva(models.Model):
     viaje = models.ForeignKey("viajes.Viaje", verbose_name="Viaje", null=True, blank=True, on_delete=models.CASCADE)
     usuario = models.ForeignKey("usuarios.Usuario", verbose_name='Pasajero', on_delete=models.CASCADE)
     estado = models.DateField(verbose_name="Estado", default=False)
