3ef26d6dc080dc1e446f15fb685b778999ded298
vagrant <vagrant@debian10.localdomain>
2021-09-24 00:22:46 +0000

    Arreglar un error en el archivo de urls en la app de vehiculos

--- reito/vehiculos/urls.py
+++ reito/vehiculos/urls.py
@@ -5,7 +5,7 @@ app_name = 'vehiculos'
 
 urlpatterns = [
     path('nuevo/', views.VehiculoCrear.as_view(), name='nuevo'),
-    path('editar/<int:pk>', views.NuevoVehiculo.as_view(), name='editar'),
-    path('eliminar/<int:pk>', views.NuevoVehiculo.as_view(), name='eliminar'),
-    path('ver/<int:pk>', views.NuevoVehiculo.as_view(), name='ver'),
+    path('editar/<int:pk>', views.VehiculoActualizar.as_view(), name='editar'),
+    path('eliminar/<int:pk>', views.VehiculoEliminar.as_view(), name='eliminar'),
+    path('ver/<int:pk>', views.VehiculoDetalle.as_view(), name='ver'),
 ]
