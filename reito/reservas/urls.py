e210d5f9e86a4e76dc4f990b497b8e033b9dec66
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-27 10:15:25 -0500

    Agregar funcion para cancelar una reserva

--- reito/reservas/urls.py
+++ reito/reservas/urls.py
@@ -5,4 +5,5 @@ app_name="reservas"
 
 urlpatterns = [
     path('nueva/<int:user_pk>/<int:viaje_pk>', views.nueva_reserva, name="nueva"),
+    path('cancelar/<int:user_pk>/<int:viaje_pk>', views.cancelar_reserva, name="cancelar"),
 ]
