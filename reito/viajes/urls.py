21b1efb16cacfa5a533e70f9492ec1991be85ddb
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-09-27 18:33:05 -0500

    Unificar ver detalle de viaje, ver detalle viaje viajero

--- reito/viajes/urls.py
+++ reito/viajes/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('editar/<int:pk>', views.EditarViaje.as_view(), name="editar"),
     path('eliminar/<int:pk>', views.EliminarViaje.as_view(), name="eliminar"),
     path('', views.index, name="index"),
-    path('detalle-v-viajero/<int:pk>', views.DetalleViajeViajero.as_view(), name="detalle_viaje_viajero"),
     path('buscar-destinos/', views.buscar_destinos,name="buscar_destinos"),
     path('buscar-viajes/<int:pk>', views.buscar_viajes,name="buscar_viajes"),
     
