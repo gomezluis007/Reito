16a5910be580b63ec408ac1e4283b87cce651689
Oscar Leonardo Corvera Espinosa <35166865@uaz.edu.mx>
2021-09-28 16:40:20 -0500

    Agrego el script de destinos y los primeros destinos para la base de datos

---
+++ reito/script_destinos.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reito.settings')
+django.setup()
+
+from viajes.models import Destino
+
+destinos_nuevos=[]
+with open('destinos.txt') as d:
+    destinos_nuevos=[line.rstrip() for line in d]
+
+destinos_registrados=Destino.objects.all().values("nombre")
+
+destinos_registrados_plano=[]
+for destino in destinos_registrados:
+    destinos_registrados_plano.append(destino['nombre'])
+
+for destino in destinos_nuevos:
+    if destino not in destinos_registrados_plano:
+        Destino.objects.create(nombre=destino)
