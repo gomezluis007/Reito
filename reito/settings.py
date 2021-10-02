03ac74f0da1a7ee86472a106f6864657b475a49f
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-10-01 21:08:59 -0500

    Corregir imports para url en heroku

--- reito/settings.py
+++ reito/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'reito.wsgi.application'
 #     }
 # }
 
-# import dj_database_url
-# from decouple import config
+import dj_database_url
+from decouple import config
 
 DATABASES = {
     'default': dj_database_url.config(
