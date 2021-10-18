a8fe06a122785bb21db22d7c48c68bec6c5b693e
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-10-18 17:23:28 -0500

    Agregar campo de descripcion en perfil y editar perfil

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Usuario(User):
     telefono= models.BigIntegerField()
+    descripcion = models.CharField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return self.get_full_name()
