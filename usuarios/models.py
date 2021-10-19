e62991ae65b6cdb62087147afd8d14368acdd62f
Oscar Leonardo Corvera Espinosa <35166865@uaz.edu.mx>
2021-10-18 20:56:23 -0500

    Se integra la imagen de los usuarios y se pueden ver en sus respectivas templates.

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from reito import settings
 
 # Create your models here.
 
 class Usuario(User):
     telefono= models.BigIntegerField()
+    foto=models.FileField(default=None,upload_to="img/",blank=True,null=True)
 
     def __str__(self):
         return self.get_full_name()
